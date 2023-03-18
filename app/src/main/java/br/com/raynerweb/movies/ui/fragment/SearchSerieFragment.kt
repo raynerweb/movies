3f6ee87021921fa706a7d9e734d92842ada8e889
raynerweb <raynerweb@gmail.com>
2023-03-18 11:48:56 +0000

    setting up main structure

---
+++ app/src/main/java/br/com/raynerweb/movies/ui/fragment/SearchSerieFragment.kt
@@ -0,0 +1,20 @@
+package br.com.raynerweb.movies.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.raynerweb.movies.R
+
+class SearchSerieFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_serie, container, false)
+    }
+
+}
