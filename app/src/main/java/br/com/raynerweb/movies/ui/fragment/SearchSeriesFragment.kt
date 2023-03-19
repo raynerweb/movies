01f241c6474e12e7b2245bc3ed66a9ce8dd68a83
raynerweb <raynerweb@gmail.com>
2023-03-19 17:10:29 +0000

    added menu filter, added searchbar

--- app/src/main/java/br/com/raynerweb/movies/ui/fragment/SearchSeriesFragment.kt
+++ app/src/main/java/br/com/raynerweb/movies/ui/fragment/SearchSeriesFragment.kt
@@ -1,11 +1,13 @@
 package br.com.raynerweb.movies.ui.fragment
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import br.com.raynerweb.movies.R
 import br.com.raynerweb.movies.databinding.FragmentSearchSeriesBinding
 import br.com.raynerweb.movies.ui.adapter.TVSeriesAdapter
 import br.com.raynerweb.movies.ui.viewmodel.SearchSeriesViewModel
@@ -38,6 +40,8 @@ class SearchSeriesFragment : Fragment() {
     }
 
     private fun setupViews() {
+        setupToolbar()
+
 //        tvSeriesAdapter = TVSeriesAdapter(mutableListOf())
 //        binding.rvTvSeries.adapter = tvSeriesAdapter
     }
@@ -48,6 +52,35 @@ class SearchSeriesFragment : Fragment() {
             binding.rvTvSeries.adapter = TVSeriesAdapter(it)
         }
 
+    }
+
+    private fun setupToolbar() {
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_filter, menu)
+
+        val searchItem: MenuItem = menu.findItem(R.id.action_search)
+        val searchManager =
+            requireActivity().getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = searchItem.actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(requireActivity().componentName))
+
+        searchView.apply {
+            maxWidth = Integer.MAX_VALUE
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    // TODO to be implemented
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    return true
+                }
+            })
+        }
     }
 
 }
